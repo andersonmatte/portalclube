359afa8360e6e488198afa2d59cc422715b6e5cc
a.tamborim <a.tamborim@tjrs.jus.br>
2019-09-12 13:44:07 -0300

    Initial commit

---
+++ src/test/java/br/com/spring/portalclube/PortalclubeApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.spring.portalclube;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortalclubeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
